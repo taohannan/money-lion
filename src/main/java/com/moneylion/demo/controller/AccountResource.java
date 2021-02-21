600a8d8449c34bcbd3dd11314d1a239ebcff5e15
Hannan Isa <taohannan@gmail.com>
2021-02-22 02:02:08 +0800

    feat - add endpoint to check features

--- src/main/java/com/moneylion/demo/controller/AccountResource.java
+++ src/main/java/com/moneylion/demo/controller/AccountResource.java
@@ -1,21 +1,39 @@
 package com.moneylion.demo.controller;
 
+import com.moneylion.demo.controller.vm.ResponseDto;
+import com.moneylion.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class AccountResource {
 
     private final Logger log = LoggerFactory.getLogger(AccountResource.class);
+    private UserService userService;
+
+    public AccountResource(UserService userService) {
+        this.userService = userService;
+    }
+
 
     @GetMapping("/test-server")
     public String testServer(){
         return "Server is up";
     }
 
+    @GetMapping("/feature")
+    public ResponseEntity<ResponseDto> checkAccess(@RequestParam(name = "email") String email,
+                                                   @RequestParam(name = "featureName") String featureName){
+
+        Boolean canAccess = userService.checkFeatureAccess(email, featureName);
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setCanAccess(canAccess);
+
+        return ResponseEntity.ok(responseDto);
+
+
+    }
 
 }
---
+++ src/main/java/com/moneylion/demo/controller/vm/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.moneylion.demo.controller.vm;
+
+public class ResponseDto {
+    private Boolean canAccess;
+
+    public Boolean getCanAccess() {
+        return canAccess;
+    }
+
+    public void setCanAccess(Boolean canAccess) {
+        this.canAccess = canAccess;
+    }
+}
